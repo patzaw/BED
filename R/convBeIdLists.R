ae81eb9548fcceb9d2fc21709ff76e42b68418c4
Patrice Godard <patrice.godard@ucb.com>
2018-03-22 15:21:56 +0100

    Convert to entities instead of identifiers

--- R/convBeIdLists.R
+++ R/convBeIdLists.R
@@ -1,6 +1,10 @@
 #' Converts lists of BE IDs
 #'
 #' @param idList a list of IDs lists
+#' @param entity if TRUE returns BE instead of BEID (default: FALSE).
+#' BE CAREFUL, THIS INTERNAL ID IS NOT STABLE AND CANNOT BE USED AS A REFERENCE.
+#' This internal identifier is useful to avoid biases related to identifier
+#' redundancy. See \url{../doc/BED.html#3_managing_identifiers}
 #' @param ... params for the \code{\link{convBeIds}} function
 #'
 #' @return a list of \code{\link{convBeIds}} ouput ids
@@ -20,12 +24,19 @@
 #' @export
 #'
 convBeIdLists <- function(
-    idList,
-    ...
+   idList,
+   entity=FALSE,
+   ...
 ){
-    ct <- convBeIds(unique(unlist(idList)), ...)
-    return(lapply(
-        idList,
-        function(x) setdiff(unique(ct$to[which(ct$from %in% x)]), NA)
-    ))
+   ct <- convBeIds(unique(unlist(idList)), ...)
+   return(lapply(
+      idList,
+      function(x){
+         if(entity){
+            setdiff(unique(ct$to.entity[which(ct$from %in% x)]), NA)
+         }else{
+            setdiff(unique(ct$to[which(ct$from %in% x)]), NA)
+         }
+      }
+   ))
 }
