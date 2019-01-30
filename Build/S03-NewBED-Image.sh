4f3da6929bb07bc165a6eb60ead0a4c250c3f5fa
Patrice Godard <patrice.godard@ucb.com>
2019-01-30 16:02:19 +0100

    Allow removing Neo4j transaction files

--- Build/S03-NewBED-Image.sh
+++ Build/S03-NewBED-Image.sh
@@ -1,12 +1,43 @@
 #/bin/bash
 
-export BED_VERSION=2018.10.07
-export NJ_VERSION=3.4.7
+export BED_VERSION=2019.01.30
+export NJ_VERSION=3.4.12
 export CONTAINER=new_bed
 export BED_DATA=`pwd`/working/neo4jData
 
 # Stop container
 docker stop $CONTAINER
+
+# Remove neostore.transaction.db. ?
+echo ""
+echo "Do you want to remove Neo4j transaction files [y,n]"
+read rmtf
+if [ $rmtf = "y" ]; then
+   echo ""
+	echo "DELETE: transaction files are going to be deleted using sudo privileges"
+   sudo rm $BED_DATA/data/databases/graph.db/neostore.transaction.db.*
+	if [ $? -eq 0 ]; then
+	      echo ""
+        	echo "Files deleted"
+        	echo ""
+	else
+	      echo ""
+        	echo "Cannot delete files (exit)" >&2
+        	echo ""
+        	exit
+	fi
+else
+	if [ $rmtf = "n" ]; then
+	   echo ""
+		echo "NOT DELETE: transaction files are going to be kept"
+		echo ""
+	else
+      echo ""
+      echo "Please chose between 'y' and 'n' (exit)" >&2
+      echo ""
+      exit
+	fi
+fi
 
 # Create and save the image
 echo "FROM neo4j:$NJ_VERSION" > $BED_DATA/Dockerfile
