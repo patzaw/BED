a4ac09caead2b02413f7ee751654ecff96daea92
Patrice Godard <patrice.godard@ucb.com>
2019-01-30 10:51:10 +0100

    Check that the neo4jData folder does not exist before instanciating a new BED db

--- Build/S01-NewBED-Container.sh
+++ Build/S01-NewBED-Container.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export NJ_VERSION=3.4.7
+export NJ_VERSION=3.4.12
 
 export BED_HTTP_PORT=5410
 export BED_BOLT_PORT=5610
@@ -8,7 +8,11 @@ export BED_BOLT_PORT=5610
 export BED_IMPORT=`pwd`/working/neo4jImport
 mkdir -p $BED_IMPORT
 export BED_DATA=`pwd`/working/neo4jData
-mkdir -p $BED_DATA
+if test -e $BED_DATA; then
+   echo "$file directory exists ==> abort - Remove it before proceeding" >&2
+   exit
+fi
+mkdir $BED_DATA
 
 export CONTAINER=new_bed
 
