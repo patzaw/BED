9bdd42a25029a612969e579c02a02516dc3c09e7
Patrice Godard <patrice.godard@gmail.com>
2022-11-06 06:56:58 +0100

    Path to rstudio pandoc

--- supp/Build/S02-Rebuild-BED.sh
+++ supp/Build/S02-Rebuild-BED.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 # R -e "Sys.setenv(PATH=paste(Sys.getenv('PATH'), '/usr/lib/rstudio-server/bin/pandoc', sep=':')); rmarkdown::render('Rebuild-BED.Rmd')";
-R -e "Sys.setenv(PATH=paste(Sys.getenv('PATH'), '/usr/lib/rstudio-server/bin/quarto/bin', sep=':')); rmarkdown::render('Rebuild-BED.Rmd')";
+R -e "Sys.setenv(PATH=paste(Sys.getenv('PATH'), '/usr/lib/rstudio-server/bin/quarto/bin', sep=':')); Sys.setenv(RSTUDIO_PANDOC='/usr/lib/rstudio-server/bin/pandoc'); rmarkdown::render('Rebuild-BED.Rmd')";
