afdcf9b26037fa129027e96bc09c2c11f5a5129b
Patrice Godard <patrice.godard@gmail.com>
2024-01-19 14:46:59 +0100

    Push dump on zenodo

---
+++ supp/Build/helpers/zenodo_upload.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Taken from: https://github.com/jhpoelen/zenodo-upload
+#
+# Upload big files to Zenodo.
+#
+# usage: ./zenodo_upload.sh [deposition id] [filename] [--verbose|-v]
+#
+
+set -e
+
+VERBOSE=0
+if [ "$3" == "--verbose" ] || [ "$3" == "-v" ]; then
+    VERBOSE=1
+fi
+
+# strip deposition url prefix if provided; see https://github.com/jhpoelen/zenodo-upload/issues/2#issuecomment-797657717
+DEPOSITION=$( echo $1 | sed 's+^http[s]*://zenodo.org/deposit/++g' )
+FILEPATH="$2"
+FILENAME=$(echo $FILEPATH | sed 's+.*/++g')
+FILENAME=${FILENAME// /%20}
+
+BUCKET=$(curl https://zenodo.org/api/deposit/depositions/"$DEPOSITION"?access_token="$ZENODO_TOKEN" | jq --raw-output .links.bucket)
+
+if [ "$VERBOSE" -eq 1 ]; then
+    echo "Deposition ID: $DEPOSITION"
+    echo "File path: $FILEPATH"
+    echo "File name: $FILENAME"
+    echo "Bucket URL: $BUCKET"
+    echo "Uploading file..."
+fi
+
+curl --progress-bar \
+    --retry 5 \
+    --retry-delay 5 \
+    -o /dev/null \
+    --upload-file "$FILEPATH" \
+    $BUCKET/"$FILENAME"?access_token="$ZENODO_TOKEN"
